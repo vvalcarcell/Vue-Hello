d36336a800edf037c577f8b2622077dba65fa100
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-25 19:37:36 +0200

    Hi Pippo!

---
+++ script.js
@@ -0,0 +1,10 @@
+new Vue(
+    {
+        el: '#root',
+        data: {
+            text: 'Hello!',
+            img: 'https://www.gelestatic.it/thimg/KooiXx2V83iu1I-E-L6ODnvv6gE=/960x540/smart/https%3A//www.ilsecoloxix.it/image/contentid/policy%3A1.39619901%3A1607105160/image/image.jpg%3Ff%3Ddetail_558%26h%3D720%26w%3D1280%26%24p%24f%24h%24w%3Dd5eb06a'
+        }
+
+    }
+)
